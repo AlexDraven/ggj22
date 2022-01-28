2a310a8050b8431f45ab69889546df53256755ff
Alejandro <alejandro@PUNTOINI-ALEJANDRO.local>
2022-01-28 01:54:39 -0300

    remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import {app, ipcMain, BrowserWindow} from 'electron';
 
-//app.on('ready', initWindows);
 app.whenReady().then(main);
 
 import electronReload from 'electron-reload';
@@ -17,7 +16,6 @@ async function main() {
     });
 
     mainWindows.loadURL('file://' + __dirname + '/index.html');
-   // mainWindows.webContents.openDevTools();
     mainWindows.on('ready-to-show', () => {
         mainWindows.show();
     });
